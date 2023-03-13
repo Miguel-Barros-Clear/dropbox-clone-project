0ffd41b2bbaebb27b732d82c65802844a41c7282
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-13 11:23:35 -0300

    feat: delete archive of firebase

--- public/src/controller/DropboxController.js
+++ public/src/controller/DropboxController.js
@@ -30,10 +30,48 @@ class DropBoxController {
             messagingSenderId: "988115574657",
             appId: "1:988115574657:web:d53bd33eae97a8090cd280"
         };
+        firebase.initializeApp(firebaseConfig);
     }
 
     getSelection() {
         return this.listFilesEl.querySelectorAll(".selected");
+    }
+
+    removeFolderTask(ref, name) {
+        return new Promise((resolve, reject) => {
+            let folderRef = this.getFirebaseRef(ref + '/' + 'name');
+
+            folderRef.on('value', snapshot => {
+                snapshot.forEach((item) => {
+                    let data = item.val();
+                    data.key = item.key;
+
+                    if (data.type === 'folder') {
+                        this.removeFolderTask(ref + '/' + name, data.name).then(() => {
+                            resolve({
+                                fields: {
+                                    key: data.key
+                                }
+                            })
+                        }).catch((err) => {
+                            reject(err);
+                        })
+                    } else if (data.type) {
+                        this.removeFile(ref + '/' + name, data.name).then(() => {
+                            resolve({
+                                fields: {
+                                    key: data.key
+                                }
+                            })
+                        }).catch((err) => {
+                            reject(err);
+                        })
+                    }
+                })
+                folderRef.remove();
+                folderRef.off('value');
+            })
+        })
     }
 
     removeTask() {
@@ -43,15 +81,35 @@ class DropBoxController {
             let file = JSON.parse(li.dataset.file);
             let key = li.dataset.key;
 
-            let formData = new FormData()
+            promises.push(new Promise((resolve, reject) => {
 
-            formData.append('path', file.path);
-            formData.append('key', key);
-            promises.push(this.ajax('/file', 'DELETE', formData));
+                if (file.type === 'folder') {
+                    this.removeFolderTask(this.currentFolder.join('/'), file.name).then(() => {
+                        resolve({
+                            fields: {
+                                key
+                            }
+                        })
+                    })
+                } else if (file.type) {
+                    this.removeFile(this.currentFolder.join('/'), file.name).then(() => {
+                        resolve({
+                            fields: {
+                                key
+                            }
+                        })
+                    })
+                }
+            }));
 
         });
 
         return Promise.all(promises);
+    }
+
+    removeFile(ref, name) {
+        let fileRef = firebase.storage().ref(ref).child(name)
+        return fileRef.delete();
     }
 
     initEvents() {
